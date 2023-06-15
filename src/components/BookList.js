3eb832c1e4975cbdcdc04e1f6c6f58adaac1284b
vanya <xandex03@mail.ru>
2023-06-15 22:22:14 +0300

    buttonLoad/Styles

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,39 +1,3 @@
-// import React, { useEffect, useState } from "react";
-
-// const BookList = () => {
-//   const [books, setBooks] = useState([]);
-//   const [data, setData] = useState({})
-
-//   useEffect(() => {
-//     fetchBooks();
-//   }, []);
-
-//   useEffect(() => 
-//   {if (data.items !== undefined) {
-//     setBooks([...data.items])}
-//   } , [data]
-//   )
-
-//   const fetchBooks = async () => {
-     
-//     const response = await fetch("https://www.googleapis.com/books/v1/volumes?q=js&key=AIzaSyByMtJ1mpylPuE9Yr9HreMl5Bxzr7FTTS0");
-//     const jsonData = await response.json();
-
-//     setData(jsonData);
-//   }
-
-//   return (
-//     <div className="book-list">
-//       {books.map((imageUrl, index) => (
-//         <img key={index} src={imageUrl?.volumeInfo?.imageLinks?.thumbnail || ""} alt="Book Cover" />
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default BookList
-
-
 import React, { useEffect, useState } from "react";
 
 const BookList = () => {
