c0cd82fee210ccc65c0ef6a97ec6be3c354cee0c
vanya <xandex03@mail.ru>
2023-06-29 00:11:21 +0300

    SortingUpdate

--- src/App.js
+++ src/App.js
@@ -33,27 +33,23 @@ const App = () => {
   };
 //&orderBy=${sorting}&printType=books&subject=${category}
 const fetchBooks = async () => {
+  const params = {
+    q: `intitle:${searchQuery}${category !== 'all' ? `+subject:${category}` : ''}`,
+    maxResults: 30,
+    startIndex: books.length || 0,
+    orderBy: sorting,
+    key: API_KEY,
+  }
+
   setLoading(true);
   try {
-    const response = await axios.get(
-      `https://www.googleapis.com/books/v1/volumes`,
-      {
-        params: {
-          q: `intitle:${searchQuery}${category !== 'all' ? `+subject:${category}` : ''}`,
-          maxResults: 30,
-          startIndex: books.length,
-          printType: 'books',
-          orderBy: sorting,
-          key: API_KEY,
-        },
-      },
-    );
-    if (response.data.items) {
-      setBooks((prevBooks) => [...prevBooks, ...response.data.items]);
-      setTotalBooks(response.data.totalItems);
-    } else {
-      setBooks([]);
-      setTotalBooks(0);
+    const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${params.q}&maxResults=30&startIndex=${params.startIndex}&orderBy=${params.orderBy}&key=${params.key}`)
+    const jsonData = await response.json();
+    console.log(jsonData);
+
+    if (jsonData.items) {
+      setBooks((prevBooks) => [...prevBooks, ...jsonData.items]);
+      setTotalBooks(jsonData.totalItems);
     }
   } catch (error) {
     console.log('Error fetching books:', error);
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -7,6 +7,10 @@ import Pagination from "./Pagination";
 const BookList = ({ books, totalBooks, handleLoadMore }) => {
   const [loading, setLoading] = useState(false);
 
+  const uniqueBooks = books.filter((book, index, arr) => {
+    return arr.findIndex((b) => b.id === book.id) === index;
+  });
+
   const handleButtonClick = () => {
     handleLoadMore();
     setLoading(true);
@@ -25,8 +29,8 @@ const BookList = ({ books, totalBooks, handleLoadMore }) => {
           <p className="totalBooks">Found 0 results</p>
         )}
           <div className="bookGrid">
-            {books.map((book, index) => (
-              <BookCard key={index} book={book} />
+            {uniqueBooks.map((book) => (
+              <BookCard key={book.id} book={book} />
             ))}
           </div>
       </div>
